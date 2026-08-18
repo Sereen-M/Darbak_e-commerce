41064a74206557ba5730d65a100c01aed8ba61c1
Abdalqader-Froukh <abdalqaderfroukh1@gmail.com>
2026-08-18 16:36:18 +0300

    initial commit

---
+++ Darbak/Darbak/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Darbak.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
